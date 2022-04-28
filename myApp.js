159f8adc5919c6c91c77c4c0717347edbbdad222
Carmen-Git-It <carmenwhitton@gmail.com>
2022-04-28 17:58:23 -0400

    Created Person model

--- myApp.js
+++ myApp.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-var mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
