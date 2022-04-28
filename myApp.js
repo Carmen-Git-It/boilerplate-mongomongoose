f9e02de97a7b66c58468a8474f93a1b84eb14978
Carmen-Git-It <carmenwhitton@gmail.com>
2022-04-28 18:02:22 -0400

    Created Person model

--- myApp.js
+++ myApp.js
@@ -3,19 +3,6 @@ var mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
-const Schema = mongoose.Schema;
-
-const personSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  age: Number,
-  favouriteFoods: [ String ]
-});
-
-const Person = mongoose.model("Person", personSchema);
-
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
@@ -64,7 +51,18 @@ const queryChain = (done) => {
   done(null /*, data*/);
 };
 
+const Schema = mongoose.Schema;
 
+const personSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favouriteFoods: [ String ]
+});
+
+const Person = mongoose.model("Person", personSchema);
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
