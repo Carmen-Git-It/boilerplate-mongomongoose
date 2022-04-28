72779726a72ef31d69caafe114acf6ea05e9eb3e
Carmen-Git-It <carmenwhitton@gmail.com>
2022-04-28 19:42:52 -0400

    Implemented FindOneAndUpdate

--- myApp.js
+++ myApp.js
@@ -97,7 +97,14 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, { new: true}, (err, person) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      done(null, person);
+    }
+  });
 };
 
 const removeById = (personId, done) => {
