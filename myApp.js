7b619e61668d9c49ad71d55f4b983a14b31ef263
Carmen-Git-It <carmenwhitton@gmail.com>
2022-04-28 18:10:54 -0400

    Created Person model

--- myApp.js
+++ myApp.js
@@ -1,7 +1,24 @@
 require('dotenv').config();
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, (err, db) => {
+  if (err) {
+    console.log("Error connecting to database.");
+  }
+});
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favouriteFoods: [ String ]
+});
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
@@ -50,19 +67,6 @@ const queryChain = (done) => {
 
   done(null /*, data*/);
 };
-
-const Schema = mongoose.Schema;
-
-const personSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  age: Number,
-  favouriteFoods: [ String ]
-});
-
-const Person = mongoose.model("Person", personSchema);
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
